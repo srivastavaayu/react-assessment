67c0c5d51f89ebb2ab7a8526bf29c7be9dd7630f
Aayush Srivastava <aayu.2312@gmail.com>
2021-05-31 20:31:30 +0530

    Use placeholder image

--- src/snippets/Courses.js
+++ src/snippets/Courses.js
@@ -1,44 +1,8 @@
 import Header from "./Header";
 import "../css/All.css";
 import "../css/Courses.css";
+import Placeholder from "../images/placeholder.png";
 import BrilliantCourseIntroduction from "../images/Brilliant-CourseIntroduction.png";
-import BrilliantIntroductionWarmupPuzzles from "../images/Brilliant-IntroductionWarmupPuzzles.png";
-import BrilliantIntroductionTruthSeeking from "../images/Brilliant-IntroductionTruthSeeking.png";
-import BrilliantIntroductionStrategicDeductions from "../images/Brilliant-IntroductionStrategicDeductions.png";
-import BrilliantTheRationalDetectiveRiddlesofOrder from "../images/Brilliant-TheRationalDetectiveRiddlesofOrder.png";
-import BrilliantTheRationalDetectiveCraftyCounting from "../images/Brilliant-TheRationalDetectiveCraftyCounting.png";
-import BrilliantTheRationalDetectiveMysteryContainers from "../images/Brilliant-TheRationalDetectiveMysteryContainers.png";
-import BrilliantTheRationalDetectiveFutoshiki from "../images/Brilliant-TheRationalDetectiveFutoshiki.png";
-import BrilliantTheRationalDetectiveShuffles from "../images/Brilliant-TheRationalDetectiveShuffles.png";
-import BrilliantTheRationalDetectiveFalseInformation from "../images/Brilliant-TheRationalDetectiveFalseInformation.png";
-import BrilliantPuzzlesandRiddlesWerewolves from "../images/Brilliant-PuzzlesandRiddlesWerewolves.png";
-import BrilliantPuzzlesandRiddlesLogicalLanguage from "../images/Brilliant-PuzzlesandRiddlesLogicalLanguage.png";
-import BrilliantPuzzlesandRiddlesIfAthenB from "../images/Brilliant-PuzzlesandRiddlesIfAthenB.png";
-import BrilliantPuzzlesandRiddlesEliminationGrids from "../images/Brilliant-PuzzlesandRiddlesEliminationGrids.png";
-import BrilliantPuzzlesandRiddlesJigsawMap from "../images/Brilliant-PuzzlesandRiddlesJigsawMap.png";
-import BrilliantMultilevelThinkingPerfectInformation from "../images/Brilliant-MultilevelThinkingPerfectInformation.png";
-import BrilliantMultilevelThinkingHatColors from "../images/Brilliant-MultilevelThinkingHatColors.png";
-import BrilliantMultilevelThinkingHatColorswithMorePeople from "../images/Brilliant-MultilevelThinkingHatColorswithMorePeople.png";
-import BrilliantMultilevelThinkingRedEyedDragons from "../images/Brilliant-MultilevelThinkingRedEyedDragons.png";
-import BrilliantMultilevelThinkingLogicalPirates from "../images/Brilliant-MultilevelThinkingLogicalPirates.png";
-import BrilliantMultilevelThinkingKnightsandKnaves from "../images/Brilliant-MultilevelThinkingKnightsandKnaves.png";
-import BrilliantCompetitiveGamesPoisonedChocolate from "../images/Brilliant-CompetitiveGamesPoisonedChocolate.png";
-import BrilliantCompetitiveGamesLukTsutKi from "../images/Brilliant-CompetitiveGamesLukTsutKi.png";
-import BrilliantCompetitiveGamesPuppiesandKittens from "../images/Brilliant-CompetitiveGamesPuppiesandKittens.png";
-import BrilliantCompetitiveGamesPoisonedChocolateII from "../images/Brilliant-CompetitiveGamesPoisonedChocolateII.png";
-import BrilliantCompetitiveGamesGameSearchAlgorithms from "../images/Brilliant-CompetitiveGamesGameSearchAlgorithms.png";
-import BrilliantLogicMachinesTruthTables from "../images/Brilliant-LogicMachinesTruthTables.png";
-import BrilliantLogicMachinesBasicLogicGates from "../images/Brilliant-LogicMachinesBasicLogicGates.png";
-import BrilliantLogicMachinesCombinations from "../images/Brilliant-LogicMachinesCombinations.png";
-import BrilliantLogicMachinesFredkinGates from "../images/Brilliant-LogicMachinesFredkinGates.png";
-import BrilliantLogicMachinesBinaryNumbersandAddition from "../images/Brilliant-LogicMachinesBinaryNumbersandAddition.png";
-import BrilliantLogicMachinesCreatingaMechanicalAdder from "../images/Brilliant-LogicMachinesCreatingaMechanicalAdder.png";
-import BrilliantAdvancedKnightsandKnavesIndirection from "../images/Brilliant-AdvancedKnightsandKnavesIndirection.png";
-import BrilliantAdvancedKnightsandKnavesKnightsandKnavesEnMasse from "../images/Brilliant-AdvancedKnightsandKnavesKnightsandKnavesEnMasse.png";
-import BrilliantAdvancedKnightsandKnavesJokers from "../images/Brilliant-AdvancedKnightsandKnavesJokers.png";
-import BrilliantAdvancedKnightsandKnavesOtherVariations from "../images/Brilliant-AdvancedKnightsandKnavesOtherVariations.png";
-import BrilliantAdvancedKnightsandKnavesHumansandVampires from "../images/Brilliant-AdvancedKnightsandKnavesHumansandVampires.png";
-import BrilliantAdvancedKnightsandKnavesChallenges from "../images/Brilliant-AdvancedKnightsandKnavesChallenges.png";
 import BrilliantMathematicalFundamentals from "../images/Brilliant-MathematicalFundamentals.png";
 import BrilliantComputerScienceFundamentals from "../images/Brilliant-ComputerScienceFundamentals.png";
 
@@ -48,17 +12,17 @@ let chapters = [
     description: "Put your logic to the test with these warmups!",
     modules: [
       {
-        imgsrc: BrilliantIntroductionWarmupPuzzles,
+        imgsrc: Placeholder,
         title: "Warmup Puzzles",
         description: "Get started with some logic warmups.",
       },
       {
-        imgsrc: BrilliantIntroductionTruthSeeking,
+        imgsrc: Placeholder,
         title: "Truth-Seeking",
         description: "Who or what is telling the truth?",
       },
       {
-        imgsrc: BrilliantIntroductionStrategicDeductions,
+        imgsrc: Placeholder,
         title: "Strategic Deductions",
         description:
           "Stretch the information you're given as far as it can go!",
@@ -70,33 +34,33 @@ let chapters = [
     description: "Eliminate the impossible and uncover the truth!",
     modules: [
       {
-        imgsrc: BrilliantTheRationalDetectiveRiddlesofOrder,
+        imgsrc: Placeholder,
         title: "Riddles of Order",
         description: "What order do these racers finish in?",
       },
       {
-        imgsrc: BrilliantTheRationalDetectiveCraftyCounting,
+        imgsrc: Placeholder,
         title: "Crafty Counting",
         description:
           "Learn strategies for counting that go far beyond just counting.",
       },
       {
-        imgsrc: BrilliantTheRationalDetectiveMysteryContainers,
+        imgsrc: Placeholder,
         title: "Mystery Containers",
         description: "What's in each of these mystery boxes?",
       },
       {
-        imgsrc: BrilliantTheRationalDetectiveFutoshiki,
+        imgsrc: Placeholder,
         title: "Futoshiki",
         description: "Arrange the numbers to match the inequalities.",
       },
       {
-        imgsrc: BrilliantTheRationalDetectiveShuffles,
+        imgsrc: Placeholder,
         title: "Shuffles",
         description: "Where did everything get shuffled to?",
       },
       {
-        imgsrc: BrilliantTheRationalDetectiveFalseInformation,
+        imgsrc: Placeholder,
         title: "False Information",
         description:
           "Solve problems where the people giving you hints might be lying.",
@@ -108,28 +72,28 @@ let chapters = [
     description: "Even trickier puzzles and the tools you need to solve them.",
     modules: [
       {
-        imgsrc: BrilliantPuzzlesandRiddlesWerewolves,
+        imgsrc: Placeholder,
         title: "Werewolves",
         description: "Who's a werewolf?",
       },
       {
-        imgsrc: BrilliantPuzzlesandRiddlesLogicalLanguage,
+        imgsrc: Placeholder,
         title: "Logical Language",
         description: "Explore the logical use of the words and, or, and not.",
       },
       {
-        imgsrc: BrilliantPuzzlesandRiddlesIfAthenB,
+        imgsrc: Placeholder,
         title: "If A, then B.",
         description:
           "If you do this quiz, you will learn about sentences like this one.",
       },
       {
-        imgsrc: BrilliantPuzzlesandRiddlesEliminationGrids,
+        imgsrc: Placeholder,
         title: "Elimination Grids",
         description: "Solve classic logic elimination puzzles.",
       },
       {
-        imgsrc: BrilliantPuzzlesandRiddlesJigsawMap,
+        imgsrc: Placeholder,
         title: "Jigsaw Map",
         description: "Where do the pieces go on the map?",
       },
@@ -140,37 +104,37 @@ let chapters = [
     description: "If you know that I know that you know... what then?",
     modules: [
       {
-        imgsrc: BrilliantMultilevelThinkingPerfectInformation,
+        imgsrc: Placeholder,
         title: "Perfect Information",
         description:
           "If everyone follows the same logic, can you find out what they know?",
       },
       {
-        imgsrc: BrilliantMultilevelThinkingHatColors,
+        imgsrc: Placeholder,
         title: "Hat Colors",
         description:
           "Can you figure out who can figure out the color of their hat?",
       },
       {
-        imgsrc: BrilliantMultilevelThinkingHatColorswithMorePeople,
+        imgsrc: Placeholder,
         title: "Hat Colors with More People",
         description:
           "What happens to hat puzzles when there are more hats and more people?",
       },
       {
-        imgsrc: BrilliantMultilevelThinkingRedEyedDragons,
+        imgsrc: Placeholder,
         title: "Red-Eyed Dragons",
         description:
           "Exactly how much can you learn from no one knowing anything?",
       },
       {
-        imgsrc: BrilliantMultilevelThinkingLogicalPirates,
+        imgsrc: Placeholder,
         title: "Logical Pirates",
         description:
           "Beware! Every pirate is as logical, greedy, and cutthroat as every other pirate.",
       },
       {
-        imgsrc: BrilliantMultilevelThinkingKnightsandKnaves,
+        imgsrc: Placeholder,
         title: "Knights and Knaves",
         description:
           "Solve classic truthteller-liar puzzles where someone else knows the answer.",
@@ -182,28 +146,28 @@ let chapters = [
     description: "What will your opponent do? Logic it out!",
     modules: [
       {
-        imgsrc: BrilliantCompetitiveGamesPoisonedChocolate,
+        imgsrc: Placeholder,
         title: "Poisoned Chocolate",
         description: "Don't take the last chocolate!",
       },
       {
-        imgsrc: BrilliantCompetitiveGamesLukTsutKi,
+        imgsrc: Placeholder,
         title: "Luk Tsut K'i",
         description: "Find the complete solution to a classic game.",
       },
       {
-        imgsrc: BrilliantCompetitiveGamesPuppiesandKittens,
+        imgsrc: Placeholder,
         title: "Puppies and Kittens",
         description:
           "Solve a fuzzier, livelier variant of the poisoned chocolate game.",
       },
       {
-        imgsrc: BrilliantCompetitiveGamesPoisonedChocolateII,
+        imgsrc: Placeholder,
         title: "Poisoned Chocolate II",
         description: "What happens when the chocolate comes in rectangles?",
       },
       {
-        imgsrc: BrilliantCompetitiveGamesGameSearchAlgorithms,
+        imgsrc: Placeholder,
         title: "Game Search Algorithms",
         description: "Learn how simple AI systems approach game strategy.",
       },
@@ -214,33 +178,33 @@ let chapters = [
     description: "Turn logic into arithmetic machines.",
     modules: [
       {
-        imgsrc: BrilliantLogicMachinesTruthTables,
+        imgsrc: Placeholder,
         title: "Truth Tables",
         description: "Diagram out the meanings of each logical operator.",
       },
       {
-        imgsrc: BrilliantLogicMachinesBasicLogicGates,
+        imgsrc: Placeholder,
         title: "Basic Logic Gates",
         description: "How do AND, OR, and NOT gates work?",
       },
       {
-        imgsrc: BrilliantLogicMachinesCombinations,
+        imgsrc: Placeholder,
         title: "Combinations",
         description: "Combine gates together to make complex circuits.",
       },
       {
-        imgsrc: BrilliantLogicMachinesFredkinGates,
+        imgsrc: Placeholder,
         title: "Fredkin Gates",
         description:
           "Take a peek at a gate used in biological and quantum computing.",
       },
       {
-        imgsrc: BrilliantLogicMachinesBinaryNumbersandAddition,
+        imgsrc: Placeholder,
         title: "Binary Numbers and Addition",
         description: "How does computer arithmetic work?",
       },
       {
-        imgsrc: BrilliantLogicMachinesCreatingaMechanicalAdder,
+        imgsrc: Placeholder,
         title: "Creating a Mechanical Adder",
         description:
           "Use what you've learned about logic gates to create a circuit that adds numbers.",
@@ -252,35 +216,35 @@ let chapters = [
     description: "Uncover liars on this island of logic!",
     modules: [
       {
-        imgsrc: BrilliantAdvancedKnightsandKnavesIndirection,
+        imgsrc: Placeholder,
         title: "Indirection",
         description: "Can you trick a liar into telling you the truth?",
       },
       {
-        imgsrc: BrilliantAdvancedKnightsandKnavesKnightsandKnavesEnMasse,
+        imgsrc: Placeholder,
         title: "Knights and Knaves En Masse",
         description:
           "What happens when you try to do logic at a big island party?",
       },
       {
-        imgsrc: BrilliantAdvancedKnightsandKnavesJokers,
+        imgsrc: Placeholder,
         title: "Jokers",
         description:
           "Solve tricky variants of knights and knaves puzzles where some residents can behave like either.",
       },
       {
-        imgsrc: BrilliantAdvancedKnightsandKnavesOtherVariations,
+        imgsrc: Placeholder,
         title: "Other Variations",
         description: "Twist your mind around these unusual variants.",
       },
       {
-        imgsrc: BrilliantAdvancedKnightsandKnavesHumansandVampires,
+        imgsrc: Placeholder,
         title: "Humans and Vampires",
         description:
           "Now there are four types of island resident: can you still work out the truth?",
       },
       {
-        imgsrc: BrilliantAdvancedKnightsandKnavesChallenges,
+        imgsrc: Placeholder,
         title: "Challenges",
         description:
           "Challenge yourself with our hardest selection of logic puzzles.",
